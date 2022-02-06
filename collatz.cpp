530af1bfef147754bb4e8ca5ac8953076df0abf6
sujatce <sujatce@gmail.com>
2022-02-06 01:14:00 -0800

    Saving changes

--- collatz.cpp
+++ collatz.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
+using namespace std::chrono;
+
 using namespace std;
 
-int main() {
+void normalalgorithm(int number)
+{
     int  bigterm = 0;
     int  bigtermnumber = 0;
-    //Takes each number to from 1 to 1000000 to calculate the sequence 
-    for(int j=1;j<=1000000;j++)
+
+    for(int j=1;j<=number;j++)
     {
         long long  n = j;
         int  terms = 1;
+
         //cout << j;
         while(n!=1)
         {
@@ -20,6 +25,7 @@ int main() {
             terms++;
             //cout << "->" << n;
         }
+
         //cout << "\t\t Terms = " << terms << endl;
         if(terms > bigterm)
         {
@@ -27,6 +33,62 @@ int main() {
             bigtermnumber = j;
         }
     }
-    cout<<"biggest sequence producing number is "<<bigtermnumber<<". Number of terms is "<<bigterm;
+    cout<<"Normal Algorithm - Biggest sequence producing number is "<<bigtermnumber<<". Number of terms is "<<bigterm<<endl;
+}
+
+void improvedalgorithm(int number)
+{
+    int  bigterm = 0;
+    int  bigtermnumber = 0;
+
+    int *cache = new int[number+1]; //if we declare a static array, memory exception happens, hence changed it to pointer array
+    //initialize cache
+    for(int i=0;i<number+1; i++)
+        cache[i] = -1;
+    cache[1] = 1;
+
+    for(int j=1;j<=number;j++)
+    {
+        long long  n = j;
+        int  terms = 0;
+
+        //cout << j;
+        while(n!=1 && n >= j)
+        {
+            terms++;
+            if(n%2==0)
+                n = n/2;
+            else
+                n = (3*n)+1;
+            //cout << "->" << n;
+        }
+
+        cache[j] = terms + cache[n];        
+        //cout << "\t\t Terms = " << terms << endl;
+        if(cache[j] > bigterm)
+        {
+            bigterm = cache[j];
+            bigtermnumber = j;
+        }
+    }
+    cout<<"Improved Algorithm - Biggest sequence producing number is "<<bigtermnumber<<". Number of terms is "<<bigterm<<endl;
+}
+
+int main() {
+    auto start = high_resolution_clock::now();
+    normalalgorithm(1000000);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+     cout << "Time taken by Normal Algorithm function: "
+         << duration.count() << " microseconds" << endl;
+
+    start = high_resolution_clock::now();
+    improvedalgorithm(1000000);
+    stop = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop - start);
+     cout << "Time taken by Improved Algorithm function: "
+         << duration1.count() << " microseconds" << endl;
+
+    cout<< "Algorithm is improved by "<<(duration-duration1)*100/duration1<< "%"<<endl;
     return 0;
 }
